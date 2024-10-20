ec9a43a5213ef1c5d7f77ce879264253edd15d52
jahidhasan6676 <jh18186676@gmail.com>
2024-10-20 14:25:42 +0600

    added the all file

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -7,18 +7,69 @@ import './Countries.css'
 const Countries = () => {
 
     const [countries, setCountries] = useState([]);
+    const [visitedCountry, setVisitedCountry] = useState([]);
+    const [flags, setFlags] = useState([]);
+    const [regions, setRegion] = useState([]);
+
 
     useEffect(() => {
         fetch("https://restcountries.com/v3.1/all")
         .then(res => res.json())
         .then(data => setCountries(data))
-    })
+    });
+
+
+    const handleVisitedCountry = (ami) =>{
+        console.log('add visited')
+        const newVisitedCountry = [...visitedCountry, ami];
+        setVisitedCountry(newVisitedCountry);
+    }
+
+    const handleVisitedFlag = (flag) =>{
+        console.log('add visited flag')
+        const newVisitedFlag = [...flags, flag];
+        setFlags(newVisitedFlag);
+    }
+
+    const handleRegion = (region) =>{
+        console.log('add region')
+        const newRegion = [...regions, region];
+        setRegion(newRegion)
+    }
+
+
     return (
         <div >
             <h3>Countries: {countries.length} </h3>
+            {/* show country name */}
+            <div>
+                <h3>Visited Country: {visitedCountry.length} </h3>
+                <div className="countries-details">
+                <ul>
+                    {
+                        visitedCountry.map(country => <li key={country.cca2}> {country} </li>)
+                    }
+                </ul>
+                <ol>
+                    {
+                        regions.map((region, index) => <li key={index} > {region} </li>)
+                    }
+                </ol>
+                </div>
+            </div>
+            
+            {/* show country flag */}
+            <div className="flag-container">
+                    {
+                        flags.map((flag, index )=> <img key={index} src={flag}/>)
+                    }
+            </div>
+
+           {/* show country all details */}
             <div className="country-container">
             {
-                countries.map(country => <Country key={country.cca3}  country={country}></Country>)
+    
+        countries.map(country => <Country key={country.cca3} handleVisitedCountry={handleVisitedCountry}  handleVisitedFlag={ handleVisitedFlag} handleRegion={handleRegion}    country={country}></Country>)
             }
             </div>
         </div>
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,16 +1,42 @@
+import { useState } from 'react';
 import './Country.css'
 
 
-const Country = ({country}) => {
-    console.log(country)
-    const {name, flags, population, region, area} = country;
+const Country = ({country, handleVisitedCountry, handleVisitedFlag, handleRegion}) => {
+    // console.log(country)
+    const {name, flags, population, region, area, cca3} = country;
+
+    const [visited, setVisited] = useState(false);
+
+    const handleVisit = () =>{
+        setVisited(!visited)
+    }
+    // console.log(handleVisitedFlag)
+  
+
     return (
-        <div className='country'>
-            <h4>Name: {name?.common} </h4>
+        <div className= {`country ${visited && 'visited'}`}>
+            <h4 style={{color: visited? 'goldenrod' : 'white'}}>Name: {name?.common} </h4>
             <img src={flags.png}/>
-            <p className='country-elements'>Population: {population}</p>
+            <p style={{color: visited? 'blueviolet' : 'white'}} className='country-elements'>Population: {population}</p>
             <p className='country-elements'>Region: {region}</p>
             <p className='country-elements'>Area: {area} </p>
+            <p><small className='country-elements'>Code: {cca3}</small></p>
+
+            <button onClick={()=> handleVisitedCountry(country.name.common)}>Mark Visited</button>
+
+            <button onClick={()=> handleVisitedFlag(flags.png)}>Add Flag</button>
+
+            <button onClick={()=>handleRegion(region)}>Add Region</button>
+
+            
+            <hr/>
+            <br />
+           <div className='display'>
+           <button onClick={handleVisit}> {visited ? 'Visited' : 'Going'}</button>
+           <p style={{color: visited? 'green' : 'white'}} className='country-elements'>{visited ? ' I am visited this country' : 'I want visit'} </p>
+           </div>
+           
         </div>
     );
 };
